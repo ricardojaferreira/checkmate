229fc226a510194d2cd9ee851dc34f92e49c35f0
ricarraf <ricarraf@gmail.com>
2017-11-28 17:39:54 +0000

    update database

--- database/checkmate.sql
+++ database/checkmate.sql
@@ -8,6 +8,21 @@ CREATE TABLE users (
   user_description VARCHAR
 );
 
+CREATE TABLE category (
+  category_id INTEGER PRIMARY KEY,
+  category_name VARCHAR,
+  user_id INTEGER REFERENCES users
+);
+
+CREATE TABLE todo (
+  todo_id INTEGER PRIMARY KEY,
+  todo_description VARCHAR,
+  todo_lastUpdate VARCHAR,
+  todo_deadline VARCHAR,
+  todo_percentage BOOLEAN,
+  category_id INTEGER REFERENCES category
+);
+
 -- All passwords are 1234 in SHA-1 format
 INSERT INTO users VALUES (NULL, "ricardo ferreira", "ricardo", "rpassword", "ricardo@email.com", "915342522", "hello world!");
 INSERT INTO users VALUES (NULL, NULL, "Filipe", "fpasswrod", NULL, NULL, NULL);
